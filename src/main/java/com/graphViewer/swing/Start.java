e43c4cae6e38c4b7357efdd406303969a1672f43
vrushali <vrushali.satpute@gmail.com>
2018-03-24 10:27:49 +1100

    application: adding default graphstreamrendrer.

--- src/main/java/com/graphViewer/swing/Start.java
+++ src/main/java/com/graphViewer/swing/Start.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 public class Start {
 
     public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
